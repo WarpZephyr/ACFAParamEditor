f55a74c16d6a094cc770d9322338c2bbb1abc9c3
natsudragneelthefiredragon <warpzephyr@gmail.com>
2023-01-26 09:31:19 -0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ArmoredCorePDR
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.EditParamsBtn = new System.Windows.Forms.Button();
+            this.ConvertDefsBtn = new System.Windows.Forms.Button();
+            this.TopBarPanel = new System.Windows.Forms.Panel();
+            this.ParamSelectorPanel = new System.Windows.Forms.Panel();
+            this.ParamDGV = new System.Windows.Forms.DataGridView();
+            this.RowSelectorPanel = new System.Windows.Forms.Panel();
+            this.CellEditorPanel = new System.Windows.Forms.Panel();
+            this.ParamRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.TopBarPanel.SuspendLayout();
+            this.ParamSelectorPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ParamDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EditParamsBtn
+            // 
+            this.EditParamsBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.EditParamsBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.EditParamsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EditParamsBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.EditParamsBtn.Location = new System.Drawing.Point(13, 13);
+            this.EditParamsBtn.Name = "EditParamsBtn";
+            this.EditParamsBtn.Size = new System.Drawing.Size(75, 23);
+            this.EditParamsBtn.TabIndex = 0;
+            this.EditParamsBtn.Text = "Edit Params";
+            this.EditParamsBtn.UseVisualStyleBackColor = false;
+            this.EditParamsBtn.Click += new System.EventHandler(this.EditParamsBtn_click);
+            // 
+            // ConvertDefsBtn
+            // 
+            this.ConvertDefsBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.ConvertDefsBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.ConvertDefsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConvertDefsBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.ConvertDefsBtn.Location = new System.Drawing.Point(94, 13);
+            this.ConvertDefsBtn.Name = "ConvertDefsBtn";
+            this.ConvertDefsBtn.Size = new System.Drawing.Size(90, 23);
+            this.ConvertDefsBtn.TabIndex = 2;
+            this.ConvertDefsBtn.Text = "Convert Defs";
+            this.ConvertDefsBtn.UseVisualStyleBackColor = false;
+            // 
+            // TopBarPanel
+            // 
+            this.TopBarPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.TopBarPanel.Controls.Add(this.ConvertDefsBtn);
+            this.TopBarPanel.Controls.Add(this.EditParamsBtn);
+            this.TopBarPanel.Controls.Add(this.RowSelectorPanel);
+            this.TopBarPanel.ForeColor = System.Drawing.SystemColors.Control;
+            this.TopBarPanel.Location = new System.Drawing.Point(0, -1);
+            this.TopBarPanel.Name = "TopBarPanel";
+            this.TopBarPanel.Size = new System.Drawing.Size(711, 45);
+            this.TopBarPanel.TabIndex = 3;
+            // 
+            // ParamSelectorPanel
+            // 
+            this.ParamSelectorPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ParamSelectorPanel.Controls.Add(this.ParamDGV);
+            this.ParamSelectorPanel.ForeColor = System.Drawing.SystemColors.Control;
+            this.ParamSelectorPanel.Location = new System.Drawing.Point(0, 44);
+            this.ParamSelectorPanel.Name = "ParamSelectorPanel";
+            this.ParamSelectorPanel.Size = new System.Drawing.Size(237, 406);
+            this.ParamSelectorPanel.TabIndex = 4;
+            // 
+            // ParamDGV
+            // 
+            this.ParamDGV.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.ParamDGV.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ParamDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ParamDGV.Location = new System.Drawing.Point(0, 55);
+            this.ParamDGV.Name = "ParamDGV";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.NullValue = "Null - You shouldn\'t be able to see this";
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ParamDGV.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.ParamDGV.Size = new System.Drawing.Size(237, 351);
+            this.ParamDGV.TabIndex = 0;
+            // 
+            // RowSelectorPanel
+            // 
+            this.RowSelectorPanel.Location = new System.Drawing.Point(31, 42);
+            this.RowSelectorPanel.Name = "RowSelectorPanel";
+            this.RowSelectorPanel.Size = new System.Drawing.Size(237, 406);
+            this.RowSelectorPanel.TabIndex = 5;
+            // 
+            // CellEditorPanel
+            // 
+            this.CellEditorPanel.Location = new System.Drawing.Point(474, 44);
+            this.CellEditorPanel.Name = "CellEditorPanel";
+            this.CellEditorPanel.Size = new System.Drawing.Size(237, 406);
+            this.CellEditorPanel.TabIndex = 6;
+            // 
+            // ParamRichTextBox
+            // 
+            this.ParamRichTextBox.Location = new System.Drawing.Point(328, 50);
+            this.ParamRichTextBox.Name = "ParamRichTextBox";
+            this.ParamRichTextBox.Size = new System.Drawing.Size(371, 388);
+            this.ParamRichTextBox.TabIndex = 7;
+            this.ParamRichTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.ParamRichTextBox);
+            this.Controls.Add(this.CellEditorPanel);
+            this.Controls.Add(this.ParamSelectorPanel);
+            this.Controls.Add(this.TopBarPanel);
+            this.Name = "MainForm";
+            this.Text = "Armored Core For Answer Param Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.TopBarPanel.ResumeLayout(false);
+            this.ParamSelectorPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ParamDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EditParamsBtn;
+        private System.Windows.Forms.Button ConvertDefsBtn;
+        private System.Windows.Forms.Panel TopBarPanel;
+        private System.Windows.Forms.Panel ParamSelectorPanel;
+        private System.Windows.Forms.Panel RowSelectorPanel;
+        private System.Windows.Forms.Panel CellEditorPanel;
+        private System.Windows.Forms.DataGridView ParamDGV;
+        private System.Windows.Forms.RichTextBox ParamRichTextBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using SoulsFormats;
+
+namespace ArmoredCorePDR
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void EditParamsBtn_click(object sender, EventArgs e)
+        {
+            // Prompt the user for folders containing files
+            CommonOpenFileDialog binFolderPathDialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = "C:\\Users",
+                IsFolderPicker = true,
+                Title = "Select the Folder containing your Params"
+            };
+
+            CommonOpenFileDialog defFolderPathDialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = "C:\\Users",
+                IsFolderPicker = true,
+                Title = "Select the Folder containing your Defs - Temporary until I make them into XMLs"
+            };
+
+            if (binFolderPathDialog.ShowDialog() != CommonFileDialogResult.Ok || defFolderPathDialog.ShowDialog() != CommonFileDialogResult.Ok)
+            {
+                return;
+            }
+
+            var binFolderPath = binFolderPathDialog.FileName;
+            var defFolderPath = defFolderPathDialog.FileName;
+
+
+            // Create lists and add data to lists
+            List <PARAMDEF> defList = new List<PARAMDEF>();
+
+            string[] defFiles = Directory.GetFiles(defFolderPath, "*.def");
+            foreach (string defPath in defFiles)
+            {
+                try
+                {
+                    defList.Add(PARAMDEF.Read(defPath));
+                }
+                catch
+                {
+                    Debug.WriteLine($"Failed to parse Paramdef at {defPath}");
+                }
+            }
+
+            List <PARAM> paramList = new List<PARAM>();
+
+            string[] binFiles = Directory.GetFiles(binFolderPath, "*.bin");
+            foreach (string binPath in binFiles)
+            {
+                try
+                {
+                    var param = PARAM.Read(binPath);
+                    param.ApplyParamdefCarefully(defList);
+                    paramList.Add(param);
+                }
+                catch 
+                {
+                    Debug.WriteLine($"Failed to parse Param at {binPath}");
+                }
+            }
+
+            // Process gathered data
+            foreach (PARAM param in paramList) 
+            {
+                ParamRichTextBox.AppendText("\r\n");
+                ParamRichTextBox.AppendText($"Param: {param}");
+                ParamRichTextBox.AppendText("\r\n");
+                foreach (var row in param.Rows)
+                {
+                    ParamRichTextBox.AppendText("\r\n");
+                    ParamRichTextBox.AppendText($"Row ID: {row.ID} Row Name: {row.Name}");
+                    ParamRichTextBox.AppendText("\r\n");
+                    try 
+                    {
+                        if (row.Cells == null)
+                            Debug.WriteLine($"Cell at {row} is null");
+                        if (row.Cells != null)
+                            foreach (var cell in row.Cells)
+                            {
+                                //ParamDGV.Rows.Add(cell.Value);
+                                ParamRichTextBox.AppendText($"{cell.Def.DisplayName}: {cell.Value}");
+                                ParamRichTextBox.AppendText("\r\n");
+                            }
+                    }
+                    catch
+                    {
+                        Debug.WriteLine($"Failed to parse cell at {row.ID} in {param}");
+                    }
+                }
+            }
+        }
+    }
+}
